ca10ba62269e8f3dd612ea254c0666eb5fdf3d6c
StenyDay <zmey9797@yandex.ru>
2016-05-10 19:12:00 +0600

    Add Graph Prog

---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Вес и масса",
+            "Время",
+            "Скорость",
+            "Объём",
+            "Длина",
+            "Площадь"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 25);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(205, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(96, 239);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(122, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(15, 75);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(133, 21);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(15, 124);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(114, 21);
+            this.comboBox3.TabIndex = 4;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Выберите систему";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 59);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(106, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Исходная величина";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 108);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Искомая величина";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(155, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Введите известное значение";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 174);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(152, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(87, 33);
+            this.button2.TabIndex = 10;
+            this.button2.Text = "Перевести";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(151, 266);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 23);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "Выход";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(105, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(105, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Итоговое значение";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(230, 301);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ WindowsFormsApplication3/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+          if(comboBox1.SelectedIndex==0)
+            {
+                comboBox2.Items.Add("Миллиграмм");
+                comboBox2.Items.Add("Грамм");
+                comboBox2.Items.Add("Килограмм");
+                comboBox2.Items.Add("Центнер");
+                comboBox2.Items.Add("Тонна");
+                comboBox2.Items.Add("Карат");
+                comboBox2.Items.Add("Русский фунт");
+                comboBox2.Items.Add("Английский фунт");
+                comboBox2.Items.Add("Пуд");
+            }
+            else if(comboBox1.SelectedIndex == 1)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Год");
+                comboBox2.Items.Add("Неделя");
+                comboBox2.Items.Add("Сутки");
+                comboBox2.Items.Add("Час");
+                comboBox2.Items.Add("Минута");
+                comboBox2.Items.Add("Секунда");
+                
+            }
+          else if (comboBox1.SelectedIndex == 2)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Км/с");
+                comboBox2.Items.Add("м/с");
+                comboBox2.Items.Add("км/ч");
+                comboBox2.Items.Add("м/ч");
+            }
+          else if (comboBox1.SelectedIndex == 3)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Кубический метр");
+                comboBox2.Items.Add("Кубический дециметр");
+                comboBox2.Items.Add("Кубический сантиметр");
+                comboBox2.Items.Add("Кубический миллиметр");
+                comboBox2.Items.Add("Литр");
+            }
+          else if (comboBox1.SelectedIndex == 4)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Километр");
+                comboBox2.Items.Add("Метр");
+                comboBox2.Items.Add("Дециметр");
+                comboBox2.Items.Add("Сантиметр");
+                comboBox2.Items.Add("Миллиметр");
+                comboBox2.Items.Add("Фут");
+                comboBox2.Items.Add("Ярд");
+            }
+          else if (comboBox1.SelectedIndex == 5)
+            {
+                comboBox2.Items.Clear();
+                comboBox2.Items.Add("Квадратный километр");
+                comboBox2.Items.Add("Гектар");
+                comboBox2.Items.Add("Дектар");
+                comboBox2.Items.Add("Квадратный метр");
+                comboBox2.Items.Add("Квадратный сантиметр");
+                comboBox2.Items.Add("Квадратный миллиметр");
+            }
+            if (comboBox1.SelectedIndex == 0)
+            {
+                comboBox3.Items.Add("Миллиграмм");
+                comboBox3.Items.Add("Грамм");
+                comboBox3.Items.Add("Килограмм");
+                comboBox3.Items.Add("Центнер");
+                comboBox3.Items.Add("Тонна");
+                comboBox3.Items.Add("Карат");
+                comboBox3.Items.Add("Русский фунт");
+                comboBox3.Items.Add("Английский фунт");
+                comboBox3.Items.Add("Пуд");
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Год");
+                comboBox3.Items.Add("Неделя");
+                comboBox3.Items.Add("Сутки");
+                comboBox3.Items.Add("Час");
+                comboBox3.Items.Add("Минута");
+                comboBox3.Items.Add("Секунда");
+
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Км/с");
+                comboBox3.Items.Add("м/с");
+                comboBox3.Items.Add("км/ч");
+                comboBox3.Items.Add("м/ч");
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Кубический метр");
+                comboBox3.Items.Add("Кубический дециметр");
+                comboBox3.Items.Add("Кубический сантиметр");
+                comboBox3.Items.Add("Кубический миллиметр");
+                comboBox3.Items.Add("Литр");
+            }
+            else if (comboBox1.SelectedIndex == 4)
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Километр");
+                comboBox3.Items.Add("Метр");
+                comboBox3.Items.Add("Дециметр");
+                comboBox3.Items.Add("Сантиметр");
+                comboBox3.Items.Add("Миллиметр");
+                comboBox3.Items.Add("Фут");
+                comboBox3.Items.Add("Ярд");
+            }
+            else if (comboBox1.SelectedIndex == 5)
+            {
+                comboBox3.Items.Clear();
+                comboBox3.Items.Add("Квадратный километр");
+                comboBox3.Items.Add("Гектар");
+                comboBox3.Items.Add("Дектар");
+                comboBox3.Items.Add("Квадратный метр");
+                comboBox3.Items.Add("Квадратный сантиметр");
+                comboBox3.Items.Add("Квадратный миллиметр");
+            }
+
+        }
+
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+        }
+
+    }
+    }
