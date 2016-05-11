e1c3f0e7526c2591557ef9f11210e78ed25d3149
Павлова Ульяна Владимировна <ip512s15@awp11.csc.local>
2016-05-11 13:29:16 +0600

    Change Functions and add MakeFile

--- Functions/Objem.c
+++ Functions/Objem.c
@@ -7,62 +7,62 @@ void objem(int m,int l, float x){
 		
 	switch(m){
 	case 1: switch(l){
-		case 1: printf("%f",x);break;
+		case 1: printf("%f\n",x);break;
 		case 2: x*=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 3: x*=1000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 4: x*=1000000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 5: x*=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 }
 		break;
 	case 2: switch(l){
 		case 1: x/=1000;
-			printf("%f",x);break;
-		case 2: printf("%f",x);break;
+			printf("%f\n",x);break;
+		case 2: printf("%f\n",x);break;
 		case 3: x*=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 4: x*=1000000;
-			printf("%f",x);break;
-		case 5:	printf("%f",x);break;
+			printf("%f\n",x);break;
+		case 5:	printf("%f\n",x);break;
 		}
 		break;
 	case 3: switch (l){
 		case 1: x/=1000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 2: x/=1000;
-			printf("%f",x);break;
-		case 3: printf("%f",x);break;
+			printf("%f\n",x);break;
+		case 3: printf("%f\n",x);break;
 		case 4: x*=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 5: x/=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 	} 
 	break;
 	case 4:	switch(l){
 		case 1: x/1000000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 2: x/1000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 3: x/1000;
-			printf("%f",x);break;
-		case 4: printf("%f",x);break;
+			printf("%f\n",x);break;
+		case 4: printf("%f\n",x);break;
 		case 5: x/=1000000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 	}
 	break;
 	case 5: switch(l){
 		case 1: x/=1000;
-			printf("%f",x);break;
-		case 2: printf("%f",x);break;
+			printf("%f\n",x);break;
+		case 2: printf("%f\n",x);break;
 		case 3: x*=1000;
-			printf("%f",x);break;
+			printf("%f\n",x);break;
 		case 4: x*=1000000;
-		case 5: printf("%f",x);break;
+		case 5: printf("%f\n",x);break;
 	}
 	break;
 		}
-break;
-}
+
+ }
--- Functions/Square.c
+++ Functions/Square.c
@@ -1,124 +1,131 @@
 #include <stdio.h>
 #include <locale.h>
-  
-void square(int m, int l,float x){
-	
 
-		
-	switch(m){
-	case 1: switch(l){
-		case 1: printf("%f",x);break;
-		case 2: x*=100;
-				printf("%f",x);break;
-		case 3: x*=1000;
-				printf("%f",x);break;
-		case 4: x*=10000;
-				printf("%f",x);break;
-		case 5: x*=1000000;
-				printf("%f",x);break;
-		case 6: x*=1000000000;
-				printf("%f",x);break;
-		case 7: x*=1000000000000;
-				printf("%f",x);break;
-	}
-		break;
-	case 2: switch(l){
-		case 1: x/=100;
-				printf("%f",x);break;
-		case 2: printf("%f",x);break;
-		case 3: x*=10;
-				printf("%f",x);break;
-		case 4: x*=100;
-				printf("%f",x);break;
-		case 5: x*=10000;
-				printf("%f",x);break;
-		case 6: x*=100000000;
-				printf("%f",x);break;
-		case 7: x*=10000000000;
-				printf("%f",x);break; 
-	}
-		break;
-	case 3: switch(l){
-		case 1: x/=1000;
-				printf("%f",x);break;
-		case 2: x/=10;
-				printf("%f",x);break;
-		case 3: printf("%f",x);break;
-		case 4: x*=10;
-				printf("%f",x);break;
-		case 5: x*=1000;
-				printf("%f",x);break;
-		case 6: x*=10000000;
-				printf("%f",x);break;
-		case 7: x*=1000000000;
-				printf("%f",x);break;
-	}
-		break;
-	case 4: switch(l){
-		case 1: x/=10000;
-				printf("%f",x);break;
-		case 2: x/=100;
-				printf("%f",x);break;
-		case 3: x/=10;
-				printf("%f",x);break;
-		case 4: printf("%f",x);break;
-		case 5: x*=100;
-				printf("%f",x);break;
-		case 6: x*=1000000;
-				printf("%f",x);break;
-		case 7: x*=100000000;
-				printf("%f",x);break;
-	}
-		break;
-	case 5: switch(l){
-		case 1: x/=1000000;
-				printf("%f",x);break;
-		case 2: x/=10000;
-				printf("%f",x);break;
-		case 3: x/=1000;
-				printf("%f",x);break;
-		case 4: x/=100;
-				printf("%f",x);break;
-		case 5: printf("%f",x);break;
-		case 6: x*=10000;
-				printf("%f",x);break;
-		case 7: x*=1000000;
-				printf("%f",x);break;
-	}
-		break;
-	case 6: switch(l){
-		case 1: x/=1000000000;
-				printf("%f",x);break;
-		case 2: x/=100000000;
-				printf("%f",x);break;
-		case 3: x/=10000000;
-				printf("%f",x);break;
-		case 4: x/=1000000;
-				printf("%f",x);break;
-		case 5: x/=10000;
-				printf("%f",x);break;
-		case 6: printf("%f",x);break;
-		case 7: x*=100;
-				printf("%f",x);break;
-}
-		break;
-	case 7: switch(l){
-		case 1: x/=1000000000000;
-				printf("%f",x);break;
-		case 2: x/=10000000000;
-				printf("%f",x);break;
-		case 3: x/=1000000000;
-				printf("%f",x);break;
-		case 4: x/=100000000;
-				printf("%f",x);break;
-		case 5: x/=1000000;
-				printf("%f",x);break;
-		case 6: x/=1000;
-				printf("%f",x);break;
-		case 7: printf("%f",x);break;
-	}
-		break;
-}
-break;
+void square(int m,int l,float x)
+{
+    setlocale (LC_ALL,"Rus");
+    switch(m)
+    {
+        case 1: switch(l)
+        {
+            case 1: printf("%f\n",x);break;
+            case 2: x*=100;
+            printf("%f\n",x);break;
+            case 3: x*=1000;
+            printf("%f\n",x);break;
+            case 4: x*=10000;
+            printf("%f\n",x);break;
+            case 5: x*=1000000;
+            printf("%f\n",x);break;
+            case 6: x*=1000000000;
+            printf("%f\n",x);break;
+            case 7: x*=1000000000000;
+            printf("%f\n",x);break;
+        } break;
+        
+        case 2: switch(l)
+        {
+            case 1: x/=100;
+            printf("%f\n",x);break;
+            case 2: printf("%f\n",x);break;
+            case 3: x*=10;
+            printf("%f\n",x);break;
+            case 4: x*=100;
+            printf("%f\n",x);break;
+            case 5: x*=10000;
+            printf("%f\n",x);break;
+            case 6: x*=100000000;
+            printf("%f\n",x);break;
+            case 7: x*=10000000000;
+            printf("%f\n",x);break;
+        }
+        break;
+        
+        case 3: switch(l)
+	{
+            case 1: x/=1000;
+            printf("%f\n",x);break;
+            case 2: x/=10;
+            printf("%f\n",x);break;
+            case 3: printf("%f\n",x);break;
+            case 4: x*=10;
+            printf("%f\n",x);break;
+            case 5: x*=1000;
+            printf("%f\n",x);break;
+            case 6: x*=10000000;
+            printf("%f\n",x);break;
+            case 7: x*=1000000000;
+            printf("%f\n",x);break;
+        }
+        break;
+        case 4: switch(l)
+	{
+            case 1: x/=10000;
+            printf("%f\n",x);break;
+            case 2: x/=100;
+            printf("%f\n",x);break;
+            case 3: x/=10;
+            printf("%f\n",x);break;
+            case 4: printf("%f\n",x);break;
+            case 5: x*=100;
+            printf("%f\n",x);break;
+            case 6: x*=1000000;
+            printf("%f\n",x);break;
+            case 7: x*=100000000;
+            printf("%f\n",x);break;
+        }
+        break;
+        case 5: switch(l)
+	{
+            case 1: x/=1000000;
+            printf("%f\n",x);break;
+            case 2: x/=10000;
+            printf("%f\n",x);break;
+            case 3: x/=1000;
+            printf("%f\n",x);break;
+            case 4: x/=100;
+            printf("%f\n",x);break;
+            case 5: printf("%f\n",x);break;
+            case 6: x*=10000;
+            printf("%f\n",x);break;
+            case 7: x*=1000000;
+            printf("%f\n",x);break;
+        }
+        break;
+        case 6: switch(l)
+	{
+            case 1: x/=1000000000;
+            printf("%f\n",x);break;
+            case 2: x/=100000000;
+            printf("%f\n",x);break;
+            case 3: x/=10000000;
+            printf("%f\n",x);break;
+            case 4: x/=1000000;
+            printf("%f\n",x);break;
+            case 5: x/=10000;
+            printf("%f\n",x);break;
+            case 6: printf("%f\n",x);break;
+            case 7: x*=100;
+            printf("%f\n",x);break;
+        }
+        break;
+        case 7: switch(l)
+        {
+            case 1: x/=1000000000000;
+            printf("%f\n",x);break;
+            case 2: x/=10000000000;
+            printf("%f\n",x);break;
+            case 3: x/=1000000000;
+            printf("%f\n",x);break;
+            case 4: x/=100000000;
+            printf("%f\n",x);break;
+            case 5: x/=1000000;
+            printf("%f\n",x);break;
+            case 6: x/=1000;
+            printf("%f\n",x);break;
+            case 7: printf("%f\n",x);break;
+        }
+        break;
+    }
 
 }
