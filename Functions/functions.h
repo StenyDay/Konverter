8e7883090afd4a00c8778c3f4487be643be69fe7
Enina Ekaterina <eninaaa_katyaaa@mail.ru>
2016-05-13 23:35:49 +0600

    changes in functions.h

--- Functions/functions.h
+++ Functions/functions.h
@@ -1,7 +1,7 @@
-float vess(int m, int l,float x);
-float time(int m, int l, float x);
-float speed(int m, int l, float x);
-float objem(int m,int l, float x);
-float distance(int m, int l,float x);
-float square(int m, int l,float x);
+float vess(int from, int in,float value);
+float time(int from, int in, float value);
+float speed(int from, int in, float value);
+float objem(int from,int in, float value);
+float distance(int from, int in,float value);
+float square(int from, int in,float value);
 
